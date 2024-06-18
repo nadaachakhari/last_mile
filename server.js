a8a41ad7812c1aec663133a99363c118158a8216
nadaachakhari <nadachakhari000@gmail.com>
2024-06-18 12:45:40 +0100

    config server

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const app = require('./app');
+
+const port = process.env.PORT || 5001;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log("Listening on " + port);
+});
+
+module.exports = server;  // Export the 'server' object for use in other modules
