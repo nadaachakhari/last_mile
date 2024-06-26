bb7568f772af89e7baa1029e06834bd89f220432
menyarb <menyarmenyar101@gmail.com>
2024-06-26 12:23:49 +0200

    create folders and import client template

--- serveur/app.js
+++ serveur/app.js
@@ -10,7 +10,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+   // port: process.env.DB_PORT
 });
 
 // Connexion à la base de données
